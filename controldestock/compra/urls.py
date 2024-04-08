1287210c0d996b99f1f535ac908da10a56c37e25
johnnie-lwd <156825536+johnnie-lwd@users.noreply.github.com>
2024-04-08 10:50:15 -0300

    Delete controldestock directory

--- controldestock/compra/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('compra/proveedores', views.lista_prov, name='Proveedores'),
-    
-   
-]
